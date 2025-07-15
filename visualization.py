6255b2764537275025a6c162e044cb0868a8e20b
viktoria <viktoria@kabeli.cl>
2025-07-15 16:28:52 -0400

    fix graphic labels

--- visualization.py
+++ visualization.py
@@ -17,7 +17,6 @@ def visualize(
     ]
 
     group1_total = different_sku_review_count + different_sku_conf_count
-    heights = [group1_total, same_name_same_sku_count, similar_name_same_sku_count, diffrent_sku_different_prod]
 
     x = range(len(labels))
 
@@ -26,8 +25,8 @@ def visualize(
     ax.bar(0, same_name_same_sku_count, color='blue')
     ax.bar(1, similar_name_same_sku_count, color='purple')
     ax.bar(2, different_sku_review_count, label='100% iguales', color='green')
-    ax.bar(2, different_sku_conf_count, bottom=different_sku_review_count, label='Con duda, > 88 % de similitud',
-           color='orange')
+    ax.bar(2, different_sku_conf_count, bottom=different_sku_review_count,
+           label='Con duda, > 88 % de similitud', color='orange')
     ax.bar(3, diffrent_sku_different_prod, color='red', label='Productos totalmente distintos')
 
     ax.set_ylabel('Cantidad de coincidencias')
@@ -35,12 +34,15 @@ def visualize(
         ax.set_title(f'Resumen de coincidencias entre subempresas para {additional_name}')
     else:
         ax.set_title('Resumen de coincidencias entre subempresas')
+
     ax.set_xticks(x)
     ax.set_xticklabels(labels, rotation=10, ha='center')
     ax.legend()
 
-    for i, count in enumerate(heights):
-        ax.text(i, count + 1, str(count), ha='center', va='bottom')
+    ax.text(0, same_name_same_sku_count / 2, str(same_name_same_sku_count), ha='center', va='center')
+    ax.text(1, similar_name_same_sku_count / 2, str(similar_name_same_sku_count), ha='center', va='center')
+    ax.text(2, group1_total / 2, str(group1_total), ha='center', va='center')
+    ax.text(3, diffrent_sku_different_prod / 2, str(diffrent_sku_different_prod), ha='center', va='center')
 
     plt.tight_layout()
     plt.show()
